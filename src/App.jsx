3e2a53362d0fdc63af846f11c6a3110e2f1044ae
vladimirkhvan <vladimirkhvan23@gmail.com>
2022-09-29 11:23:29 +0500

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import { Routes, Route } from 'react-router-dom';
+
+import { Login } from './pages/Login';
+import NotFound from './pages/NotFound/NotFound';
+import { Main } from './pages/Main';
+
 function App() {
-    return <div className="App"></div>;
+    return (
+        <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/messages" element={<Main />} />
+            <Route path="*" element={<NotFound />} />
+        </Routes>
+    );
 }
 
 export default App;
